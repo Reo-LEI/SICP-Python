159e48fb0dce7160f0520a29bb8e39dc1e8081c4
Reo LEI <leinuowen@gmail.com>
2016-07-08 21:46:15 +0800

    add 2.40-2.41

--- 2.33-2.43 serial interface.py
+++ 2.33-2.43 serial interface.py
@@ -1,129 +1,290 @@
+import sys
 from orderedpair import *
 
+sys.setrecursionlimit(1000000)
+
+
 def enumerate_interval(low, high):
-    if low > high: return None
-    else: return cons(low, enumerate_interval(low+1, high))
+    if low > high:
+        return None
+    else:
+        return cons(low, enumerate_interval(low+1, high))
+
 
 def enumerate_tree(tree):
-    if tree == None: return None
-    elif not isinstance(tree, tuple): return lister(tree)
-    else: return append(enumerate_tree(car(tree)), enumerate_tree(cdr(tree)))
+    if tree is None:
+        return None
+    elif not isinstance(tree, tuple):
+        return lister(tree)
+    else:
+        return append(enumerate_tree(car(tree)), enumerate_tree(cdr(tree)))
+
 
 def map_list(func, seq):
-    if seq == None: return None
-    else: return map_list(func(car(seq)), map_list(func, cdr(seq)))
+    if seq is None:
+        return None
+    else:
+        return map_list(func(car(seq)), map_list(func, cdr(seq)))
+
 
 def filt(predicate, seq):
-    if seq == None: return None
+    if seq is None:
+        return None
     elif predicate(car(seq)):
+        return cons(car(seq), filt(predicate, cdr(seq)))
+    else:
         return filt(predicate, cdr(seq))
-    else: return filt(predicate, cdr(seq))
+
 
 def accumulate(op, init, seq):
-    if seq == None: return init
+    if seq is None:
+        return init
     else:
-        if op == 'add':return car(seq)+accumulate(op, init, cdr(seq))
-        elif op == 'mul':return car(seq)*accumulate(op, init, cdr(seq))
-        else: return op(car(seq), accumulate(op, init, cdr(seq)))
+        if op == 'add':
+            return car(seq)+accumulate(op, init, cdr(seq))
+        elif op == 'mul':
+            return car(seq)*accumulate(op, init, cdr(seq))
+        else:
+            return op(car(seq), accumulate(op, init, cdr(seq)))
 
 
 # 2.33
 def map_(p, seq):
-    return  accumulate(lambda x, y: cons(p(x), y), None, seq)
+    return accumulate(lambda x, y: cons(p(x), y), None, seq)
+
 
 def append_(seq1, seq2):
-    return  accumulate(cons, seq2, seq1)
+    return accumulate(cons, seq2, seq1)
+
 
 def length_(seq):
-    return accumulate(lambda x,y: 1, 0, seq)
+    return accumulate(lambda x, y: 1, 0, seq)
 
 
 # 2.34
-def horner_eval(x, coefficient_seuence):
+def horner_eval(x, coefficient_sequence):
     return accumulate(lambda this_coeff, higher_item: this_coeff+x*higher_item,
-                      0, coefficient_seuence)
+                      0, coefficient_sequence)
+
 
 # 2.35
 def count_leave_(tree):
     # 对树的映射
     # 若节点要叶节点，返回1组成的列表，调用accumulate累积
     # 若节点为内部节点，则调用count_leave_计算叶节点
-    return accumulate('add', 0, mapping(lambda x: 1 if not isinstance(x, tuple)
-                                                else count_leave_(x), tree))
+    return accumulate('add', 0, mapping(
+        lambda x: 1 if not isinstance(x, tuple) else count_leave_(x), tree))
+
 
 # 2.36
 def car_n(seqs):
     return mapping(car, seqs)
 
+
 def cdr_n(seqs):
     return mapping(cdr, seqs)
 
+
 def accumulate_n(op, init, seqs):
-    if car(seqs) == None: return None
-    else: return cons(accumulate(op, init, car_n(seqs)),
-                      accumulate_n(op, init, cdr_n(seqs)))
+    if car(seqs) is None:
+        return None
+    else:
+        return cons(accumulate(op, init, car_n(seqs)),
+                    accumulate_n(op, init, cdr_n(seqs)))
+
 
 # 2.37
 def dot_product(v, w):
-    return accumulate('add', 0, mapping_n(lambda x,y: x*y, v, w))
+    return accumulate('add', 0, mapping_n(lambda x, y: x*y, v, w))
+
 
 def matrix_mul_vector(m, v):
     return mapping(lambda col: dot_product(v, col), m)
 
+
 def _transpose(m):
     return mapping_n(lambda *col: lister(*col), m)
+
 
 def transpose(mat):
     return accumulate_n(cons, None, mat)
 
+
 def _matrix_mul_matrix(m, n):
-    return mapping(lambda line: mapping_n(lambda *col:
-                                          dot_product(line, lister(*col)), n), m)
+    return mapping(lambda line: mapping_n(
+        lambda *col: dot_product(line, lister(*col)), n), m)
+
 
 def matrix_mul_matrix(m, n):
     # 利用矩阵转置将n的列转为行，再利用mapping对n的各行应用点乘
-    return mapping(lambda line: mapping(lambda col:
-                                        dot_product(line, col), transpose(n)), m)
+    return mapping(lambda line: mapping(
+        lambda col: dot_product(line, col), transpose(n)), m)
+
 
 # 2.38
 def fold_right(op, init, seq):
     # 从序列最右侧[op(an, init)]开始组合（折叠）
     # 直接效果就是右侧元素结果作用于左侧元素
-    if seq == None: return init
+    if seq is None:
+        return init
     else:
-        if op == 'add':return car(seq)+fold_right(op, init, cdr(seq))
-        elif op == 'mul':return car(seq)*fold_right(op, init, cdr(seq))
-        elif op == 'div': return car(seq)/fold_right(op, init, cdr(seq))
-        else: return op(car(seq), fold_right(op, init, cdr(seq)))
+        if op == 'add':
+            return car(seq)+fold_right(op, init, cdr(seq))
+        elif op == 'mul':
+            return car(seq)*fold_right(op, init, cdr(seq))
+        elif op == 'div':
+            return car(seq)/fold_right(op, init, cdr(seq))
+        else:
+            return op(car(seq), fold_right(op, init, cdr(seq)))
+
 
 def fold_left(op, init, seq):
     # 从序列最左侧[op(a1, init)]开始组合（折叠）
     # 直接效果就是左侧元素结果作用于右侧元素
     def _iter(result, rest):
-        if rest == None: return result
+        if rest is None:
+            return result
         else:
-            if op == 'add':return _iter(result+car(rest), cdr(rest))
-            elif op == 'mul':return _iter(result*car(rest), cdr(rest))
-            elif op == 'div': return _iter(result/car(rest), cdr(rest))
-            else: return _iter(op(result, car(rest)), cdr(rest))
+            if op == 'add':
+                return _iter(result+car(rest), cdr(rest))
+            elif op == 'mul':
+                return _iter(result*car(rest), cdr(rest))
+            elif op == 'div':
+                return _iter(result/car(rest), cdr(rest))
+            else:
+                return _iter(op(result, car(rest)), cdr(rest))
     return _iter(init, seq)
+
 
 def reverse_right(seq):
     # 直接效果为作则元素作为列表加在右侧元素组成的列表末尾
     return fold_right(lambda x, y: append(y, lister(x)), None, seq)
+
 
 def reverse_left(seq):
     # 直接效果为构建逆向序列
     return fold_left(lambda x, y: cons(y, x), None, seq)
 
 
+# 嵌套映射
+def flatmap(func, seq):
+    return accumulate(append, None, mapping(func, seq))
+
+
+def prime(n,test=2):
+    if test == 2:
+        if test**2 > n:
+            return True
+        elif n % test == 0:
+            return False
+        else:
+            return prime(n, test+1)
+    else:
+        if test**2 > n:
+            return True
+        elif n % test == 0:
+            return False
+        else:
+            return prime(n, test+2)
+
+
+def prime_sum(pair):
+    if prime(car(pair)+car(cdr(pair))):
+        return True
+    else:
+        return False
+
+
+def make_pair_sum(pair):
+    return lister(car(pair), car(cdr(pair)), car(pair)+car(cdr(pair)))
+
+
+def prime_sum_pairs(n):
+    return mapping(make_pair_sum, filt(prime_sum, flatmap(
+        lambda i: mapping(lambda j: lister(i, j), enumerate_interval(1, i-1)),
+        enumerate_interval(1, n))))
+
+
+def remove(item, seq):
+    return filt(lambda x: x != item, seq)
+
+
+def permutations(s):
+    if s is None:
+        return lister(None)
+    else:
+        return flatmap(lambda x: mapping(lambda p: cons(x, p),
+                                         permutations(remove(x, s))), s)
+
+
+# 2.40
+def unique_pairs(n):
+    return flatmap(lambda i: mapping(
+        lambda j: lister(i, j), enumerate_interval(1, i-1)),
+                   enumerate_interval(1, n))
+
+
+def prime_sum_pairs_uni(n):
+    return mapping(make_pair_sum, filt(prime_sum, unique_pairs(n)))
+
+
+# 2.41
+def unique_triples(n):
+    return flatmap(lambda i:
+                   flatmap(lambda j:
+                           mapping(lambda k: lister(i, j, k),
+                                   enumerate_interval(1, j-1)),
+                           enumerate_interval(1, i-1)),
+                   enumerate_interval(1, n))
+
+
+def sum_triples(triple):
+    return car(triple)+car(cdr(triple))+car(cdr(cdr(triple)))
+
+
+def triples_sum_to_s(n, s):
+    return filt(lambda t: True if sum_triples(t) == s else False,
+                unique_triples(n))
+
+
+# 2.42
+def adjoin_position(row, col, rest):
+    return cons(cons(row, col), rest)
+
+
+def safe(col, position):
+    def checker(new, rest):
+        if rest is None:
+            return True
+        else:
+            new_k = cons(car(new)+col, car(new)-col)
+            rest_k = cons(car(car(rest))+cdr(car(rest)), car(car(rest))-cdr(car(rest)))
+            if car(new) == car(car(rest)) or car(new_k) == car(rest_k) or cdr(new_k) == cdr(rest_k):
+                return False
+            else:
+                return checker(new, cdr(rest))
+    return checker(car(position), cdr(position))
+
+
+def queens(board_size):
+    def queen_cols(k):
+        if k == 0:
+            return lister(None)  # empty_board
+        else:
+            return filt(lambda position: safe(k, position),
+                        flatmap(lambda rest_of_queens:
+                                mapping(lambda new_row:
+                                        adjoin_position(new_row, k, rest_of_queens),
+                                        enumerate_interval(1, board_size)),
+                                queen_cols(k-1)))
+    return queen_cols(board_size)
 
 
 if __name__ == '__main__':
-    list1 = lister(1,2,3)
-    list2 = lister(4,5,6)
-    list3 = lister(7,8,9)
-    list4 = lister(10,11,12)
+    list1 = lister(1, 2, 3)
+    list2 = lister(4, 5, 6)
+    list3 = lister(7, 8, 9)
+    list4 = lister(10, 11, 12)
     seqs = lister(list1, list2, list3, list4)
     tree = lister(1, lister(2, lister(3, 4), 5), lister(6, 7))
     
@@ -139,7 +300,7 @@ if __name__ == '__main__':
     v1 = lister(1, 2, 3, 4)
     v2 = lister(4, 5, 6, 6)
     v3 = lister(6, 7, 8, 9)
-    m  = lister(v1, v2, v3)
+    m = lister(v1, v2, v3)
     print(dot_product(v1, v2), '\n')
     display(matrix_mul_vector(m,v1))
     display(transpose(m))
@@ -147,7 +308,14 @@ if __name__ == '__main__':
 
     display(fold_right('div', 1, list1))
     display(fold_left('div', 1, list1))
-    print(fold_right(lister, None, list1))
-    print(fold_left(lister, None, list1))
-    print(reverse_right(list1))
-    print(reverse_left(list1))
+    print(fold_right(lister, None, list1), '\n')
+    print(fold_left(lister, None, list1), '\n')
+    print(reverse_right(list1), '\n')
+    print(reverse_left(list1), '\n')
+
+    display(prime_sum_pairs(5))
+    display(permutations(list1))
+    display(prime_sum_pairs_uni(5))
+    display(unique_triples(4))
+    display(triples_sum_to_s(13, 10))
+    for_each(display,queens(8))
--- orderedpair.py
+++ orderedpair.py
@@ -2,22 +2,26 @@ __all__ = ['cons', 'car', 'cdr', 'lister', 'list_ref', 'length', 'append',
            'last_pair', 'reverse', 'mapper', 'for_each', 'display', 'count_leave',
            'deep_reverse', 'mapper', 'mapping', 'subsets', 'mapping_n']
 
-import pdb
 
 def cons(a, b):
     return (a, b)
 
+
 def car(x):
     return x[0]
+
 
 def cdr(x):
     return x[1]
 
+
 def car_n(seqs):
     return mapping(car, seqs)
 
+
 def cdr_n(seqs):
     return mapping(cdr, seqs)
+
 
 def lister(*args):
     def conser(x, y):
@@ -34,51 +38,81 @@ def lister(*args):
 ##    elif isinstance(first, tuple): return cons(first[0], lister(first[1:]))
 ##    else: return cons(first, lister(args))
 
+
 def list_ref(item, n):
-    if n == 0: return car(item)
-    else: return list_ref(cdr(item), n-1)
+    if n == 0:
+        return car(item)
+    else:
+        return list_ref(cdr(item), n-1)
+
 
 def last_pair(item):
-    if cdr(item) == None: return car(item)
-    else: return last_pair(cdr(item))
+    if cdr(item) is None:
+        return car(item)
+    else:
+        return last_pair(cdr(item))
+
 
 def length(item):
-    if item == None: return 0
-    else: return 1+length(cdr(item))
+    if item is None:
+        return 0
+    else:
+        return 1+length(cdr(item))
+
 
 def count_leave(item):
-    if item == None: return 0
-    elif not isinstance(item, tuple): return 1
-    else: return count_leave(car(item))+count_leave(cdr(item))
+    if item is None:
+        return 0
+    elif not isinstance(item, tuple):
+        return 1
+    else:
+        return count_leave(car(item))+count_leave(cdr(item))
 
-def append(list1, list2):
-    if list1 == None: return list2
-    else: return cons(car(list1), append(cdr(list1), list2))
+
+def append(l1, l2):
+    if l1 is None:
+        return l2
+    else:
+        return cons(car(l1), append(cdr(l1), l2))
+
 
 def reverse(item):
-    if not isinstance(item, tuple): return item
-    else: return cons(reverse(cdr(item)), car(item))
+    if not isinstance(item, tuple):
+        return item
+    else:
+        return cons(reverse(cdr(item)), car(item))
+
 
 def deep_reverse(item):
-    if not isinstance(item, tuple): return item
-    else: return cons(deep_reverse(cdr(item)), deep_reverse(car(item)))
+    if not isinstance(item, tuple):
+        return item
+    else:
+        return cons(deep_reverse(cdr(item)), deep_reverse(car(item)))
+
 
 def mapping(func, item):
     # return a seq
     # 接受一个序列，依次将序列各个元素作为func的参数，返回结果序列
-    if item == None: return None
-    else: return cons(func(car(item)), mapping(func, cdr(item)))
+    if item is None:
+        return None
+    else:
+        return cons(func(car(item)), mapping(func, cdr(item)))
+
 
 def mapper(func, tree):
     # return a tree
     # 接受一个树，依次讲各个叶节点作为func的参数，返回树
-    if tree == None: pass
-    elif not isinstance(tree, tuple): return func(tree)
-    else: return cons(mapper(func, car(tree)), mapper(func, cdr(tree)))
+    if tree is None:
+        pass
+    elif not isinstance(tree, tuple):
+        return func(tree)
+    else:
+        return cons(mapper(func, car(tree)), mapper(func, cdr(tree)))
+
 
 def mapping_n(func, *seqs):
     # return a seq
-    # 接受n个序列，依次取各个序列第k元素组成序列seq(k)作为func的参数，返回结果序列 
+    # 接受n个序列，依次取各个序列第k元素组成序列seq(k)作为func的参数，返回结果序列
     def car_n(seq):
         return mapping(car, seq)
     
@@ -87,39 +121,53 @@ def mapping_n(func, *seqs):
 
     def seq_trans(seq):  # 将序列转化为元组
         def translator(s):
-            if cdr(s) == None: return [car(s)]
-            else: return [car(s)]+translator(cdr(s))
+            if cdr(s) is None:
+                return [car(s)]
+            else:
+                return [car(s)]+translator(cdr(s))
         return tuple(translator(seq))
         
     def _map(f, seq):
-        if car(seq) == None: return None
-        else: return cons(f(*seq_trans(car_n(seq))), _map(f, cdr_n(seq)))  # 用*解包元组调用f
+        if car(seq) is None:
+            return None
+        else:
+            return cons(f(*seq_trans(car_n(seq))), _map(f, cdr_n(seq)))  # 用*解包元组调用f
 
-    if len(seqs) > 1: seqs = lister(*seqs)  # 判断seqs为树或者多个列表并解包
-    else: seqs = seqs[0]  # 若为树直接解包
+    if len(seqs) > 1:
+        seqs = lister(*seqs)  # 判断seqs为树或者多个列表并解包
+    else:
+        seqs = seqs[0]  # 若为树直接解包
     return _map(func, seqs)
     
 
 def for_each(proc, item):
-    if item == None: pass
+    if item is None:
+        pass
     else:
         proc(car(item))
         for_each(proc, cdr(item))
+
 
 def subsets(s):
     # 递归思路:
     # 集合{a,b,c}的子集=集合{b,c}的子集+集合{b,c}的子集与集合{a}的并集
     # 集合{b,c}的子集=集合{c}的子集+集合{c}的子集与集合{b}的并集
     # 集合{c}的子集={}和{c}
-    if s == None: return lister(None)
-    else: return append(subsets(cdr(s)), mapping(lambda x: cons(car(s), x), subsets(cdr(s))))
+    if s is None:
+        return lister(None)
+    else:
+        return append(subsets(cdr(s)), mapping(lambda x: cons(car(s), x), subsets(cdr(s))))
+
 
 def display(items):
     
     def order(item):
-        if item == None: return False
-        elif not isinstance(item, tuple): return True
-        else: return order(car(item))
+        if item is None:
+            return False
+        elif not isinstance(item, tuple):
+            return True
+        else:
+            return order(car(item))
 
     if order(items):
         
@@ -129,7 +177,7 @@ def display(items):
                 # 直接返回元素，
                 return item
 
-            elif cdr(item) == None:
+            elif cdr(item) is None:
                 # 嵌套列表叶子为(x, None)形式，由show(cdr(item))处理
                 # 避免None再次引用递归，提前省去并除去None标识符
                 # [car(item)]为与其余元素合并作准备
@@ -156,60 +204,59 @@ def display(items):
     else:
             
         def nested(item):
-            if item == None: return False
-            elif isinstance(cdr(item), tuple) and car(item) == None: return True
-            else: return nested(car(item))
+            if item is None:
+                return False
+            elif isinstance(cdr(item), tuple) and car(item) is None:
+                return True
+            else:
+                return nested(car(item))
     
         def show(item):
             if not isinstance(item, tuple):
                 return item
-            elif car(item) == None:
+            elif car(item) is None:
                 return [cdr(item)]  
             elif not isinstance(car(item), tuple):
                 return tuple([show(car(item))]+[show(cdr(item))])  
             elif nested(item):
                 return [show(cdr(car(item)))]+[show(cdr(item))] 
-            else:return show(car(item))+[show(cdr(item))]
+            else:
+                return show(car(item))+[show(cdr(item))]
         
     print(show(items), '\n')
     return show(items)
 
 if __name__ == '__main__':
-    display(lister(1,2,3,4))
+    display(lister(1, 2, 3, 4))
     
-    x = cons(lister(1, 2),lister(3, 4))
+    x = cons(lister(1, 2), lister(3, 4))
     display(x)
-##    print(x, length(x), count_leave(x), '\n')
+#    print(x, length(x), count_leave(x), '\n')
 
-    y = lister(lister(1, 2),lister(3, 4))
+    y = lister(lister(1, 2), lister(3, 4))
     display(y)
-##    print(y, length(y), count_leave(y), '\n')
+#    print(y, length(y), count_leave(y), '\n')
 
     z = lister(cons(1, 2), cons(3, 4), 5, 6)
     display(z)
-##    print(z, length(z), count_leave(z), '\n')
+#    print(z, length(z), count_leave(z), '\n')
 
-    p = lister(1,lister(2,lister(3, 4)))
+    p = lister(1, lister(2, lister(3, 4)))
     display(p)
-##    print(p, length(p), count_leave(p), '\n')
+#    print(p, length(p), count_leave(p), '\n')
     
     list1 = lister(1, 2, 3)
     list2 = lister(4, 5, 6)
     list3 = lister(7, 8, 9)
-    listtest = lister(list1, list2, list3)
-    display(listtest)
-##    print(listtest, '\n')
-##    print(reverse(listtest), '\n')
-    r = deep_reverse(listtest)
+    testlist = lister(list1, list2, list3)
+    display(testlist)
+#    print(listtest, '\n')
+#    print(reverse(listtest), '\n')
+    r = deep_reverse(testlist)
     k = deep_reverse(p)
-##    print(deep_reverse(listtest), '\n')
-##    print(k, '\n')
+#    print(deep_reverse(listtest), '\n')
+#    print(k, '\n')
     display(r)
     display(k)
-    display(mapper(lambda x: x**2, listtest))
-    display(mapping_n(lambda x,y,z: x+y+z, list1, list2, list3))
-                  
-    
-                          
-                          
-                          
+    display(mapper(lambda x: x**2, testlist))
+    display(mapping_n(lambda x, y, z: x+y+z, list1, list2, list3))
